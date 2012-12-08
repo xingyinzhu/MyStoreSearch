16e2012c1575e5d728f9866982121ad180c388d5
XingyinZhu <xingyinzhu@gmail.com>
2012-12-08 14:36:23 +0800

    03 - Using Web Service

--- MyStoreSearch/SearchResult.m
+++ MyStoreSearch/SearchResult.m
@@ -13,4 +13,16 @@
 @synthesize name = _name;
 @synthesize artistName = _artistName;
 
+@synthesize artworkURL60 = _artworkURL60;
+@synthesize artworkURL100 = _artworkURL100;
+@synthesize storeURL = _storeURL;
+@synthesize kind = _kind;
+@synthesize currency = _currency;
+@synthesize price = _price;
+@synthesize genre = _genre;
+
+- (NSComparisonResult)compareName:(SearchResult *)other
+{
+    return [self.name localizedStandardCompare:other.name];
+}
 @end
